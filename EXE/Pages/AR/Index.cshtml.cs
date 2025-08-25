2c6af7f68192a532f66746533acff4e7af6c8d45
tranhuytoan161104 <176930448+tranhuytoan161104@users.noreply.github.com>
2025-08-25 14:39:15 +0700

    Change AR location from /Index to /AR/Index

---
+++ EXE/Pages/AR/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EXE.Pages.AR
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
